f8974ef2a8ca3d840ef6ff582bb7225ecc664e65
Arjun <iamarjunkharal@gmail.com>
2023-03-09 16:28:49 +0545

    decorators and oop

--- oop/privateExample.py
+++ oop/privateExample.py
@@ -1,18 +1,34 @@
-# in this code we will learn about private access in python 
+#in this code wwe will study about private access of class
 
 class BankAccount:
-    def __init__(self,accountHolder):
-        self._balance = 0 # here balance method have single _ so it is private access
-        self._name = accountHolder # only the code inside class can obtain data 
-            with open(self._name + 'Ledger.txt','w') as ledgerfile:
-                ledgerfile.write('\n balance is 0\n')
-               
-    def deposite(self,amount):
+    def __init__(self,accountholder):
+        self._balance = 0 
+        self._name = accountholder
+        with open(self._name + 'ledger.txt','w') as ledgerfile:
+            ledgerfile.write("balance is 0\n")
+
+    def deposit(self,amount):
         if amount <= 0:
-            return print("we don't allow negative ''deposite'' ")
-        with open(self.name + 'Ledger.txt','w') as ledgerfile:
-            ledgerfile.write('Deposit' + str(amount) +'\n')
-            ledgerfile.write('balance is ' + str(self._balance) + '\n')
+            return 'deposits'
+        self._balance += amount 
+        with open(self._name+'ledger.txt','w') as ledgerfile:
+            ledgerfile.write('amount that is ' + str(amount) + '\n')
+            ledgerfile.write('balance is' + str(self._balance)+ '\n')
 
+    def withdraw(self,amount):
+        if self._balance < amount or amount < 0 :
+            return 'not enough in account or amount is to low to deposit'
+        self._balance -= amount 
+        with open(self._name+'ledger.txt','w') as ledgerfile:
+            ledgerfile.write('amount that is withdraw ' + str(amount) + '\n')
+            ledgerfile.write(f'balance is your accout is {self._balance} \n')
     
+account1 = BankAccount('alice')
+account1.deposit(999)
+account1.withdraw(500)
 
+account1._balance = 1000000000000 #well changing _balance outside the class is impolite
+account1.withdraw(100)            #but we can. 
+
+account1._name = 'bob' #without creating object i.e bob,we can explicitly edit name,bob.txt
+account1.withdraw(1022)
