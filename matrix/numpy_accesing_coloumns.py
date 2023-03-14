07212dbfc6b93f31d3d6464379520a5fb7551364
Arjun <iamarjunkharal@gmail.com>
2023-03-14 19:46:30 +0545

    update

---
+++ matrix/numpy_accesing_coloumns.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+print('the first coloumns is ',a[:,0])  # first coloumn of all rows
+# a[a:b] to understand this , think it elemens from a to b 
+print("to print second coulom is",a[:,1])
+print('to print 3rd coloumn is',a[:,2])
+
